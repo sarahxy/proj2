ae93a92b3e4c02c8470a8d9315775b4a203cdfcf
Shawn Huang <shawnhuang1992@gmail.com>
2015-04-27 14:55:54 -0700

    Shawn: added damage mechanism

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -7,13 +7,37 @@ class CharactersController < ApplicationController
   end
 
   def damage
-    @victim = Character.find(params[:id]) # The Character being attacked
-    @victim.health -= 10
-    $player = @victim.player_id # Saves the Player being attacked for the redirect
-    @victim.save
+    $victim = Character.find(params[:id]) # Saves the Character being attacked
+    $player = $victim.player_id # Saves the Player being attacked for the redirect
   end
 
   def level_up # Called after damage.html.erb form
+    # Extracts the elements used for the attacking and victim characters
+    attacker_element = character_params[:element]
+    victim_element = ["Earth", "Wind", "Fire", "Water"].sample
+
+    # Generates the attacking character's damage multiplier (0.7 to 1.3)
+    critical = Random.new
+    critical_damage = critical.rand(0.7..1.3)
+  
+    # Determines the attacking character's element multiplier
+    element_multiplier = 1
+    if ((attacker_element == "Water") and (victim_element == "Fire")) or
+       ((attacker_element == "Fire") and (victim_element == "Earth")) or
+       ((attacker_element == "Earth") and (victim_element == "Wind")) or
+       ((attacker_element == "Wind") and (victim_element == "Water"))
+       element_multiplier = 2
+    end
+
+    # Calculates the attacking character's total damage
+    base_damage = 10
+    total_damage = base_damage * element_multiplier * critical_damage
+
+    # Updates the victim character's health after the damage is done
+    $victim.health -= total_damage
+    $victim.save
+
+    # Updates the attacking character's experience after the damage is done
     attacker = Character.find_by(name: character_params[:name])
     attacker.experience += 10 # Attacker gains experience
     if attacker.experience >= 100  # If Attacker EXP goes to 100
@@ -21,7 +45,9 @@ class CharactersController < ApplicationController
       attacker.experience = 0      # And their EXP resets
     end
     attacker.save
-    redirect_to Player.find($player) # Redirects to page of the attacked Player
+
+    # Redirects to page of the attacked Player
+    redirect_to Player.find($player)
   end
 
   def heal
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,37 @@
 
 ActiveRecord::Schema.define(version: 20150424003350) do
 
+  create_table "characters", force: true do |t|
+    t.string   "name"
+    t.integer  "level"
+    t.integer  "player_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "health"
+    t.integer  "strength"
+    t.integer  "magic"
+    t.integer  "experience"
+  end
+
+  create_table "players", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name"
+  end
+
+  add_index "players", ["email"], name: "index_players_on_email", unique: true
+  add_index "players", ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true
+
   create_table "beasts", force: true do |t|
     t.string   "name"
     t.string   "type"
